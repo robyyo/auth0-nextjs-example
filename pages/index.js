25ac87f01e697822b521d7512c2cabce4046c9e8
Robert Finkley <me@robertfinkley.com>
2022-10-31 15:18:55 -0400

    added dynamic logged-in elements

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Home() {
+  const { user } = useUser();
+
   return (
     <div>
       <Head>
@@ -15,6 +18,15 @@ export default function Home() {
           This is a simple example of authentication and authorization using the
           Auth0 platform by Okta.
         </p>
+        {user ? (
+          <div>
+            <h2>{user.nickname} is logged in!</h2>
+          </div>
+        ) : (
+          <div>
+            <h2>You are not currently logged in.</h2>
+          </div>
+        )}
       </main>
     </div>
   );
--- pages/profile.js
+++ pages/profile.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import { useUser } from '@auth0/nextjs-auth0';
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import useSWR from 'swr';
 
-export default function Profile() {
+const fetcher = async (uri) => {
+  const response = await fetch(uri);
+  return response.json();
+};
+
+export default withPageAuthRequired(function Profile() {
+  const { data, err } = useSWR('/api/protected', fetcher);
   const { user, error, isLoading } = useUser();
-
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
+
+  if (err) return <div>oops... {err.message}</div>;
+  if (data === undefined) return <div>Loading...</div>;
+
+  console.log(data);
 
   return (
     user && (
       <div>
-        <img src={user.picture} alt={user.name} />
+        <img src={user.picture} alt={user.name} height="100" />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
+        <p>
+          <code>/profile/</code> is a protected route. This page can only be
+          accessed while logged in.
+        </p>
+        <p>Session Info: {data.protected}</p>
       </div>
     )
   );
-}
+});
